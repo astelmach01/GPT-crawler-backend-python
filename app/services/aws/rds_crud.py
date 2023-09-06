b33380ee567c02a10d13c2f6feb86afb894b32ba
astelmach01 <astelmach20@gmail.com>
2023-09-05 18:40:43 -0700

    added db rollback

--- app/services/aws/rds_crud.py
+++ app/services/aws/rds_crud.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from functools import wraps
 
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
@@ -8,7 +9,27 @@ from .models import Task as TaskModel
 from .models import User as UserModel  # Import models from models.py
 
 
+def handle_db_errors(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        session = None
+        for arg in args:
+            if isinstance(arg, Session):
+                session = arg
+                break
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            if session:
+                session.rollback()
+            logging.error(f"Error in DB operation: {e}")
+            return None
+
+    return wrapper
+
+
 # User
+@handle_db_errors
 def create_user(name: str, session: Session) -> UserModel:
     logging.info(f"Creating user with name: {name}")
     new_user = UserModel(name=name)
@@ -17,14 +38,17 @@ def create_user(name: str, session: Session) -> UserModel:
     return new_user
 
 
+@handle_db_errors
 def read_users(session: Session) -> list[UserModel]:
     return session.query(UserModel).all()
 
 
+@handle_db_errors
 def read_user_by_id(user_id: int, session: Session) -> UserModel | None:
     return session.query(UserModel).filter_by(id=user_id).first()
 
 
+@handle_db_errors
 def update_user(user_id: int, new_name: str, session: Session) -> UserModel | None:
     user = read_user_by_id(user_id, session)
     if not user:
@@ -35,6 +59,7 @@ def update_user(user_id: int, new_name: str, session: Session) -> UserModel | No
     return user
 
 
+@handle_db_errors
 def delete_user(user_id: int, session: Session) -> UserModel | None:
     user = read_user_by_id(user_id, session)
     if not user:
@@ -45,6 +70,7 @@ def delete_user(user_id: int, session: Session) -> UserModel | None:
 
 
 # Task
+@handle_db_errors
 def create_task(
     description: str, date: datetime, user_id: int, session: Session
 ) -> TaskModel | None:
@@ -59,19 +85,23 @@ def create_task(
     return new_task
 
 
+@handle_db_errors
 def read_tasks(session: Session) -> list[TaskModel]:
     return session.query(TaskModel).all()
 
 
+@handle_db_errors
 def read_task_by_id(task_id: int, session: Session) -> TaskModel | None:
     return session.query(TaskModel).filter_by(id=task_id).first()
 
 
+@handle_db_errors
 def read_tasks_by_user_id(user_id: int, session: Session) -> list[TaskModel]:
     logging.info(f"Reading tasks for user with id: {user_id}")
     return session.query(TaskModel).filter_by(user_id=user_id).all()
 
 
+@handle_db_errors
 def update_task(
     task_id: int,
     session: Session,
@@ -97,6 +127,7 @@ def update_task(
     return task
 
 
+@handle_db_errors
 def delete_task(task_id: int, session: Session) -> TaskModel | None:
     task = read_task_by_id(task_id, session)
     if not task:

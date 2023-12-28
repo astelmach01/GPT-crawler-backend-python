1f50069c551faaeeadf412bb257206eb50c5f113
astelmach01 <astelmach20@gmail.com>
2023-12-28 18:50:55 -0500

    Remove unused settings and variables

--- app/settings.py
+++ app/settings.py
@@ -27,30 +27,6 @@ class Settings(BaseSettings):
     with environment variables.
     """
 
-    OPENAI_API_KEY: str
-    OPENAI_ORGANIZATION: str
-    MODEL_NAME: str = "gpt-3.5-turbo"
-
-    # AWS
-    AWS_ACCESS_KEY: str
-    AWS_SECRET_ACCESS_KEY: str
-    AWS_REGION: str
-
-    # DB
-    DB_USERNAME: str
-    DB_PASSWORD: str
-    DB_ENDPOINT: str
-    DB_PORT: int
-
-    JWT_SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-
-    # get url for aws rds mysql
-    def get_db_url(self, db_name) -> str:
-        return (
-            f"mysql+pymysql://{self.DB_USERNAME}:{self.DB_PASSWORD}"
-            f"@{self.DB_ENDPOINT}:{self.DB_PORT}/{db_name}"
-        )
 
     # Frontend URL
     frontend_url: str
@@ -66,31 +42,6 @@ class Settings(BaseSettings):
     # Current environment
     ENVIRONMENT: str = "development"
 
-    # Variables for RabbitMQ
-    rabbit_host: str = "app-rmq"
-    rabbit_port: int = 5672
-    rabbit_user: str = "guest"
-    rabbit_pass: str = "guest"
-    rabbit_vhost: str = "/"
-
-    rabbit_pool_size: int = 2
-    rabbit_channel_pool_size: int = 10
-
-    @property
-    def rabbit_url(self) -> URL:
-        """
-        Assemble RabbitMQ URL from settings.
-
-        :return: rabbit URL.
-        """
-        return URL.build(
-            scheme="amqp",
-            host=self.rabbit_host,
-            port=self.rabbit_port,
-            user=self.rabbit_user,
-            password=self.rabbit_pass,
-            path=self.rabbit_vhost,
-        )
 
     log_level: LogLevel = LogLevel.INFO
 

978aabb2285ccc14476f07c71a9ebd735a4b358c
astelmach01 <astelmach20@gmail.com>
2024-01-07 23:39:51 -0500

    Add selenium to requirements and update make_assistant function

--- app/web/api/core/crawl.py
+++ app/web/api/core/crawl.py
@@ -1,141 +1,106 @@
+import hashlib
 import logging
-import multiprocessing
-import re
-from datetime import datetime
-from pathlib import Path
+import os
 from typing import Set
 from urllib.parse import urlparse
-from urllib.parse import urlunparse
 
-import scrapy
-from bs4 import BeautifulSoup
-from pydantic import HttpUrl
-from scrapy import signals
-from scrapy.crawler import CrawlerProcess
-from scrapy.exceptions import CloseSpider
-from scrapy.signalmanager import dispatcher
-from scrapy.utils.project import get_project_settings
-
-from app import OUTPUT_DIR
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-settings = get_project_settings()
-settings.set("ROBOTSTXT_OBEY", False)
-
-
-def normalize_url(url):
+def format_filename(url):
     parsed_url = urlparse(url)
-    normalized_path = re.sub(r"/{2,}", "/", parsed_url.path).rstrip("/")
-    normalized_url = urlunparse(
-        (
-            parsed_url.scheme,
-            parsed_url.netloc,
-            normalized_path,
-            "",  # params
-            "",  # query
-            "",  # fragment
-        )
+    formatted_name = parsed_url.netloc + parsed_url.path
+    return formatted_name.strip("/").replace("/", "_").replace("-", "_")
+
+
+def fetch_page(driver, url, current_depth, max_depth, output_dir, visited):
+    if url in visited or current_depth > max_depth:
+        return
+    visited.add(url)
+
+    driver.get(url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "body"))
     )
-    return normalized_url
 
+    parsed_url = urlparse(url)
+    file_name = format_filename(url) + "_text.txt"
+    file_path = os.path.join(output_dir, file_name)
 
-class MySpider(scrapy.Spider):
-    name = "my_spider"
+    # Extract text content from the body of the page
+    text_content = driver.find_element(By.TAG_NAME, "body").get_attribute("innerText")
 
-    def __init__(self, start_url: str, depth_limit: int, *args, **kwargs) -> None:
-        super(MySpider, self).__init__(*args, **kwargs)
-        self.start_urls = [start_url]
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text_content)
 
-        self.visited_urls: Set = set()
-        self.depth_limit = depth_limit
+    logging.info(
+        f"Text content saved from {url} at depth {current_depth} to {file_path}"
+    )
 
-    def parse(self, response):  # noqa
-        # Extract domain from start_url
-        domain = urlparse(self.start_urls[0]).netloc
-        domain_path = OUTPUT_DIR / domain.replace(".", "_")
-        domain_path.mkdir(parents=True, exist_ok=True)
-
-        for href in response.css("a::attr(href)").getall():
-            absolute_url = response.urljoin(href)
-            normalized_url = normalize_url(absolute_url)
-
-            # Check if the URL belongs to the same domain and hasn't been visited
-            if (
-                urlparse(normalized_url).netloc == domain
-                and normalized_url not in self.visited_urls
-            ):
-                self.visited_urls.add(normalized_url)
-                self.log(
-                    f"Visited URL: {normalized_url},"
-                    f" total unique urls visited: {len(self.visited_urls)}"
+    # Collecting all links before visiting
+    if current_depth < max_depth:
+        hrefs = [
+            link.get_attribute("href")
+            for link in driver.find_elements(By.TAG_NAME, "a")
+        ]
+        for href in hrefs:
+            if href and urlparse(href).netloc == parsed_url.netloc:
+                fetch_page(
+                    driver, href, current_depth + 1, max_depth, output_dir, visited
                 )
 
-                if len(self.visited_urls) >= self.depth_limit:
-                    raise CloseSpider(f"Reached depth limit of {self.depth_limit}")
 
-                # Save page content to a file
-                filename = (
-                    f"{Path(normalized_url).name}"
-                    f"-{datetime.now().strftime('%Y%m%d%H%M%S%f')}.txt"
-                )
-                file_path = OUTPUT_DIR / domain.replace(".", "_") / filename
+def create_master_file(url, output_dir):
+    formatted_name = format_filename(url)
+    domain_path = os.path.join(output_dir, formatted_name)
+    master_file_path = os.path.join(domain_path, f"{formatted_name}_master.txt")
 
-                try:
-                    with file_path.open("w", encoding="utf-8") as file:
-                        soup = BeautifulSoup(response.text, "html.parser")
-                        text = soup.get_text()
-                        cleaned_text = re.sub(r"\s+", " ", text).strip()
-                        file.write(cleaned_text)
-                except IOError as e:
-                    self.log(f"Failed to write file {file_path}: {e}")
+    seen_hashes = set()
 
-                # Follow the link if it hasn't been visited and the depth limit
-                # hasn't been reached
-                yield scrapy.Request(normalized_url, callback=self.parse)
+    with open(master_file_path, "w", encoding="utf-8") as master_file:
+        for root, dirs, files in os.walk(domain_path):
+            for file in files:
+                if file.endswith("_text.txt"):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, "r", encoding="utf-8") as f:
+                        for line in f:
+                            line_hash = hashlib.md5(line.encode("utf-8")).hexdigest()
+                            if line_hash not in seen_hashes:
+                                master_file.write(line)
+                                seen_hashes.add(line_hash)
+                        master_file.write("\n\n")
 
-
-def run_spider(start_url: HttpUrl, depth_limit: int, *args, **kwargs):
-    def spider_closed(spider, reason):
-        create_master_file(start_url)
-
-    dispatcher.connect(spider_closed, signal=signals.spider_closed)
-
-    process = CrawlerProcess()
-
-    process.crawl(
-        MySpider, start_url=start_url, depth_limit=depth_limit, *args, **kwargs
-    )
-    process.start()
+    logging.info(f"Master file created at {master_file_path}")
 
 
-async def crawl_webpage(start_url: str, depth_limit: int = 1000, *args, **kwargs):
-    # Run Scrapy in a separate process
-    crawler_process = multiprocessing.Process(
-        target=run_spider, args=(start_url, depth_limit, *args), kwargs=kwargs
-    )
-    crawler_process.start()
-    crawler_process.join()
+async def crawl_webpage(url: str, max_depth: int = 100):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
+    domain_name = urlparse(url).netloc
+    output_dir = os.path.join("output", domain_name)
+    os.makedirs(output_dir, exist_ok=True)
 
-def create_master_file(start_url: str):
-    cleaned_url = urlparse(start_url).netloc.replace(".", "_")
-    domain_path = OUTPUT_DIR / cleaned_url
-    master_file_path = domain_path / f"{cleaned_url}_master_combined.txt"
+    visited: Set = set()
+    fetch_page(driver, url, 1, max_depth, output_dir, visited)
 
-    logging.info(f"Creating master file {master_file_path}")
+    driver.quit()
 
-    with master_file_path.open("w", encoding="utf-8") as master_file:
-        for txt_file in domain_path.glob("*.txt"):
-            try:
-                master_file.write(txt_file.read_text(encoding="utf-8"))
-            except IOError as e:
-                print(f"Failed to read file {txt_file}: {e}")
-
-    logging.info(f"Finished creating master file {master_file_path}")
+    create_master_file(url, "output")
 
 
 async def main():
-    await crawl_webpage("https://astelmach01.github.io/tinylang")
+    URL = "https://nextjs.org"
+    MAX_DEPTH = 2  # Adjust as needed
+    await crawl_webpage(URL, MAX_DEPTH)
 
 
 if __name__ == "__main__":
